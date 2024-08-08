5fa23b3d86d003d59090ff1af8f4dffd869eca20
isayvaid <vaidyasayli70@gmail.com>
2024-08-08 20:14:07 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/isayvaid",
-    resume: "https://drive.google.com/file/d/1ZuCdOKRjpBjUPfh-qMtM7Syalix399ap/view?usp=sharing",
+    resume: "https://docs.google.com/document/d/1eVQFsOLP5GzfaNll5sK338kmPtPn_4w7j4BV80mUiFg/edit?usp=sharing",
     linkedin: "https://www.linkedin.com/in/sayli-vaidya-577784221/",
   };
   export const skills = [
