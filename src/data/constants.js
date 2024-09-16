13e20e54cb894ef72467ed08c4494c8c33283995
Sayli Vaidya <92618082+isayvaid@users.noreply.github.com>
2024-09-16 18:32:05 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/isayvaid",
-    resume: "https://drive.google.com/file/d/1yv53TlbHUrs8kavPDJZrB3XHrGGZpmWd/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1XbDCj1oyoUiQsDjzREve3TpVTl4Mh70a/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/sayli-vaidya-577784221/",
   };
   export const skills = [
